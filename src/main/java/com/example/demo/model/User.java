89caf9ab353d0ae0f7606d331ae090004a2b8466
bala-amypo <bala@amypo.in>
2025-12-17 06:20:18 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,8 +1,12 @@
+import jakarta.persistance.column;
+
 public class User
 {
     private Long id;
-    private string fullName;
-    private string password;
-    
+    private String fullName;
+    private String Password;
+    @column(unqiue="true")
+    private String Email;
+
 
 }
