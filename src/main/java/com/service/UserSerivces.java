eaa000b7bfa51a95397d1934d3d962ef6e9b9ae7
bala-amypo <bala@amypo.in>
2025-12-18 07:57:51 +0000

    User commit

--- src/main/java/com/service/UserSerivces.java
+++ src/main/java/com/service/UserSerivces.java
@@ -1,3 +1,8 @@
+package com.example.demo.service;
+import com.example.demo.entity.User;
+import org.springframework.stereotype.Service;
+
+@Service
 public interface UserService{
     User register(User user);
     String  findByEmail(String email );
