9fc2994ddd1fe9aba415480faba49acf44aa29a6
bala-amypo <bala@amypo.in>
2025-12-19 09:37:55 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/VendorServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/VendorServiceImpl.java
@@ -2,6 +2,8 @@ package com.example.demo.service.implement;
 import com.example.demo.service.VendorService;
 import org.springframework.stereotype.Service;
 import com.example.demo.repository.VendorRepository;
+import java.util.List;
+
 
 @Service
 public class VendorServiceImpl implements VendorService
@@ -15,5 +17,11 @@ public class VendorServiceImpl implements VendorService
     {
         return obj.save(vendor);
     }
-    
+    public Vendor getVendor(Long id){
+        return obj.findById(id);
+    }
+    public List<Vendor> getAllVendors()
+    {
+        return obj.findAll();
+    }
 }
