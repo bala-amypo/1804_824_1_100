cd3aa272d5937c22fe10072f704ed2def0d53dd9
bala-amypo <bala@amypo.in>
2025-12-19 08:37:18 +0000

    User commit

--- src/main/java/com/example/demo/service/VendorService.java
+++ src/main/java/com/example/demo/service/VendorService.java
@@ -1,7 +1,9 @@
 package com.example.demo.service;
-
+import java.util.List;
+import com.example.demo.model.Vendor;
 public interface VendorService 
 {
     Vendor createVendor(Vendor vendor);
-    
+    Vendor getVendor(Long id);
+    List<Vendor> getAllVendors();
 }
