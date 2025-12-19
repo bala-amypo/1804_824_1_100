da2931eb4b646543fab37736f300087d98fe442d
bala-amypo <bala@amypo.in>
2025-12-19 03:56:13 +0000

    User commit

--- src/main/java/com/example/demo/model/Vendor.java
+++ src/main/java/com/example/demo/model/Vendor.java
@@ -1,8 +1,12 @@
 package com.example.demo.model;
 import jakarta.persistence.id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
 public class Vendor{
  @Id
  @GenerateValue(status=GenerationType.IDENTITY)
- private id
+ private Long id;
+ @Column (unique=true)
+
 }
