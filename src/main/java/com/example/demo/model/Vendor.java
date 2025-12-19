b3bb772c99d1a0d1b5006fc3775c6b8010d829c4
bala-amypo <bala@amypo.in>
2025-12-19 04:02:09 +0000

    User commit

--- src/main/java/com/example/demo/model/Vendor.java
+++ src/main/java/com/example/demo/model/Vendor.java
@@ -3,7 +3,9 @@ import jakarta.persistence.id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import java.time.LocalDateTime;
+import jakarta.persistence.Entity;
 
+@Entity
 public class Vendor{
  @Id
  @GenerateValue(status=GenerationType.IDENTITY)
@@ -15,6 +17,21 @@ public class Vendor{
  private String industry;
  private LocalDateTime createdAt;
 
- 
+ public Long getId()
+ {
+    return id;
+ }
+ public void setId(Long id)
+ {
+    this.id=id;
+ }
+ public String getVendorName()
+ {
+    return vendorName;
+ }
+ public void setVendorName()
+ {
+    
+ }
 
 }
