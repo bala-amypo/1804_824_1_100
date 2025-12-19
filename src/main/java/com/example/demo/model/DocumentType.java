5d7e3724ede3ca6a7d5da6a449c8525f9828ec15
bala-amypo <bala@amypo.in>
2025-12-19 13:25:21 +0000

    User commit

--- src/main/java/com/example/demo/model/DocumentType.java
+++ src/main/java/com/example/demo/model/DocumentType.java
@@ -1,2 +1,14 @@
 package com.example.demo.model;
-import jakarta.persistence.NE
+import jakarta.persistence.Enitity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType
+@Entity
+public class DocumentType
+{
+    @Id
+    @GeneratedValue(Strategy=GenerationType.IDENTITY)
+    
+
+}
