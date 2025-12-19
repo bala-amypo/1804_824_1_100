26bf70e9ccee172fa93275fea8f42b7abc878d91
bala-amypo <bala@amypo.in>
2025-12-19 08:46:18 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/VendorServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/VendorServiceImpl.java
@@ -1,10 +1,14 @@
 package com.example.demo.service.implement;
 import com.example.demo.service.VendorService;
 import org.springframework.stereotype.Service;
-import com.example.demo.repository.
+import com.example.demo.repository.VendorRepository;
 
 @Service
 public class VendorServiceImpl implements VendorService
 {
     private final VendorRepository obj;
+    public VendorServiceImpl(VendorRepository obj)
+    {
+        this.obj=obj;
+    }
 }
