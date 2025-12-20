9efd55c29bb3e6b993cb05809da9d6ad01880392
bala-amypo <bala@amypo.in>
2025-12-20 04:30:13 +0000

    User commit

--- src/main/java/com/example/demo/service/DocumentTypeService.java
+++ src/main/java/com/example/demo/service/DocumentTypeService.java
@@ -1,6 +1,8 @@
 package com.example.demo.service;
 import java.util.List;
 import com.example.demo.model.DocumentType;
+
+
 public class DocumentType
 {
     DocumentType createDocumentType(DocumentType type);
---
+++ src/main/java/com/example/demo/service/impl/DocumentTypeServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.demo.service.impl;
+import com.example.demo.service.DocumentTypeService;
+import com.example.demo.repo
+import org.springframework.stereotype.Service;
+ 
+ @Service
+public interface DocumentTypeServiceImpl implements DocumentTypeService
+{
+
+}
