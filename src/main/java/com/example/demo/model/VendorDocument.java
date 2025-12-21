b836a6896f0fb4579df327ed3d8dace3975afabc
bala-amypo <bala@amypo.in>
2025-12-21 10:48:14 +0000

    User commit

--- src/main/java/com/example/demo/model/VendorDocument.java
+++ src/main/java/com/example/demo/model/VendorDocument.java
@@ -1,47 +1,49 @@
 package com.example.demo.model;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.PrePersist;
+
 import java.time.LocalDateTime;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
-import com.example.demo.model.Vendor;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.JoinColumn;
-import com.example.demo.model.documentType;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-public class DocumentType
-{
+public class DocumentType {
+
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     @ManyToOne
-    @JoinColumn(name= "vendor_id")
+    @JoinColumn(name = "vendor_id", nullable = false)
     private Vendor vendor;
-    @Column(unique=true,nullable = false)
+
+    @Column(unique = true, nullable = false)
     private String fileUrl;
-    @ManyToOne
-    @JoinColumn(name= "documentType_id")
-    private DocumentType documentType;
+
+    @Column(nullable = false)
+    private String documentType;
+
     private Boolean isValid;
+
     private LocalDateTime createdAt;
+
     private LocalDateTime expiryDate;
+
     @PrePersist
     protected void onCreate() {
-    this.createdAt = LocalDateTime.now();
-    @PrePersist
-    protected void onCreated() {
-    this.expiryDate = LocalDateTime.now();
-}
-
-
+        this.createdAt = LocalDateTime.now();
+        this.expiryDate = LocalDateTime.now();
+    }
 }
