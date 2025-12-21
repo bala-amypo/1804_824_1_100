0f84e5c06b7f20335b182bdfec18730fad0a8af2
bala-amypo <bala@amypo.in>
2025-12-21 17:00:42 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/DocumentTypeServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/DocumentTypeServiceImpl.java
@@ -1,4 +1,4 @@
-package com.example.demo.service.impl;
+`package com.example.demo.service.impl;
 import com.example.demo.service.DocumentTypeService;
 import com.example.demo.repository.DocumentTypeRepository;
 import org.springframework.stereotype.Service;
