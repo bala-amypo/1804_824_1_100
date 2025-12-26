e8b3b7cea18fc5a48037d3ae579e0e3f664bcde5
bala-amypo <bala@amypo.in>
2025-12-26 05:18:23 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/DocumentTypeServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/DocumentTypeServiceImpl.java
@@ -1,11 +1,34 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.model.DocumentType;
 import com.example.demo.repository.DocumentTypeRepository;
+import com.example.demo.service.DocumentTypeService;
+import org.springframework.stereotype.Service;
 
-public class DocumentTypeServiceImpl {
+import java.util.List;
+
+@Service   // ✅ REQUIRED
+public class DocumentTypeServiceImpl implements DocumentTypeService {
+
     private final DocumentTypeRepository repo;
 
     public DocumentTypeServiceImpl(DocumentTypeRepository repo) {
         this.repo = repo;
+    }
+
+    @Override
+    public DocumentType create(DocumentType docType) {
+        return repo.save(docType);
+    }
+
+    @Override
+    public List<DocumentType> getAll() {
+        return repo.findAll();
+    }
+
+    @Override
+    public DocumentType getById(Long id) {
+        return repo.findById(id)
+                .orElseThrow(() -> new RuntimeException("DocumentType not found"));
     }
 }
