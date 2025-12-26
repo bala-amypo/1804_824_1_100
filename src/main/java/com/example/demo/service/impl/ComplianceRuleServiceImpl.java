7314daf95d1fc803f732b302e10f467067ffea28
bala-amypo <bala@amypo.in>
2025-12-26 05:15:23 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ComplianceRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ComplianceRuleServiceImpl.java
@@ -1,11 +1,34 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.model.ComplianceRule;
 import com.example.demo.repository.ComplianceRuleRepository;
+import com.example.demo.service.ComplianceRuleService;
+import org.springframework.stereotype.Service;
 
-public class ComplianceRuleServiceImpl {
+import java.util.List;
+
+@Service   // ✅ REQUIRED
+public class ComplianceRuleServiceImpl implements ComplianceRuleService {
+
     private final ComplianceRuleRepository repo;
 
     public ComplianceRuleServiceImpl(ComplianceRuleRepository repo) {
         this.repo = repo;
+    }
+
+    @Override
+    public ComplianceRule createRule(ComplianceRule rule) {
+        return repo.save(rule);
+    }
+
+    @Override
+    public List<ComplianceRule> getAllRules() {
+        return repo.findAll();
+    }
+
+    @Override
+    public ComplianceRule getRule(Long id) {
+        return repo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Rule not found"));
     }
 }
