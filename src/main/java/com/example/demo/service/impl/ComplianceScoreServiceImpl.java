cfbd5c76f9be6f5365634620e05eda85af749bb2
bala-amypo <bala@amypo.in>
2025-12-26 08:29:32 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ComplianceScoreServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ComplianceScoreServiceImpl.java
@@ -1,29 +1,14 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.repository.VendorDocumentRepository;
-import com.example.demo.repository.VendorRepository;
-import com.example.demo.repository.VendorDocumentTypeRepository;
-import com.example.demo.service.ComplianceScoreService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ComplianceScoreServiceImpl implements ComplianceScoreService {
+public class ComplianceScoreServiceImpl {
 
-    private VendorDocumentRepository vendorDocumentRepository;
-    private VendorRepository vendorRepository;
-    private VendorDocumentTypeRepository vendorDocumentTypeRepository;
+    private final VendorDocumentRepository vendorDocumentRepository;
 
     public ComplianceScoreServiceImpl(VendorDocumentRepository vendorDocumentRepository) {
         this.vendorDocumentRepository = vendorDocumentRepository;
-    }
-
-    public ComplianceScoreServiceImpl(
-            VendorDocumentRepository vendorDocumentRepository,
-            VendorRepository vendorRepository,
-            VendorDocumentTypeRepository vendorDocumentTypeRepository
-    ) {
-        this.vendorDocumentRepository = vendorDocumentRepository;
-        this.vendorRepository = vendorRepository;
-        this.vendorDocumentTypeRepository = vendorDocumentTypeRepository;
     }
 }
