8a41151b2f5152b74f587d77ab015afe0b354d76
bala-amypo <bala@amypo.in>
2025-12-26 10:19:32 +0000

    User commit

--- src/main/java/com/example/demo/repository/ComplianceScoreRepository.java
+++ src/main/java/com/example/demo/repository/ComplianceScoreRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ComplianceScoreRepository extends JpaRepository<ComplianceScore, Long> {
+
     Optional<ComplianceScore> findByVendorId(Long vendorId);
 }
