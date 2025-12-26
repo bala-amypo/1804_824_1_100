f674ca74d86b9dc164ac320154c579807fa64552
bala-amypo <bala@amypo.in>
2025-12-26 04:17:17 +0000

    User commit

--- src/main/java/com/example/demo/servlet/HealthServlet.java
+++ src/main/java/com/example/demo/servlet/HealthServlet.java
@@ -1,4 +1,17 @@
-public class HealthServlet
-{
-    
+package com.example.demo.servlet;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/health")
+public class HealthServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getWriter().write("OK");
+    }
 }
